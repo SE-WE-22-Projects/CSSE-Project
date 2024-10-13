095ba0815674a7565d2e7d5ab1aa9ee5456d5584
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:17:00 +0530

    Service Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/model/Service.java
@@ -0,0 +1,19 @@
+package com.csse.healthSphere.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int serviceId;
+    String name;
+    String description;
+    String category;
+    float price;
+}
