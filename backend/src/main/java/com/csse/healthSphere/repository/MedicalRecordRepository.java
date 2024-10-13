e378021744d08bc4127e092ce94013bc546756cd
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:15:43 +0530

    MedicalRecord Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/repository/MedicalRecordRepository.java
@@ -0,0 +1,10 @@
+package com.csse.healthSphere.repository;
+
+import com.csse.healthSphere.model.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+
+}
