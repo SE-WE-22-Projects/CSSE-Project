e7504e6a9ddcae7d70672a86891ab3e452108aed
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:11:19 +0530

    Appointment Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/repository/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package com.csse.healthSphere.repository;
+
+import com.csse.healthSphere.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+}
