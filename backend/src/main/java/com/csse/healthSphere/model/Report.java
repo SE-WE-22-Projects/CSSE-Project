8fd6610ed984a0d2842ee2587f81be4abaf78e41
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:16:44 +0530

    Report Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/model/Report.java
@@ -0,0 +1,22 @@
+package com.csse.healthSphere.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int reportId;
+    String result;
+    String status;   // TODO: need enum
+
+    @ManyToOne
+    @JoinColumn(name = "serviceId")
+    Service service;
+
+    @ManyToOne
+    @JoinColumn(name = "patientId")
+    Patient patient;
+}
