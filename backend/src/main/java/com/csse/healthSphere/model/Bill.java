1ec6be2dc74dda5ca4c81a8bc399cf5e4a4a1f53
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:14:47 +0530

    Bill Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/model/Bill.java
@@ -0,0 +1,18 @@
+package com.csse.healthSphere.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int billId;
+
+    String status; // TODO: need enum
+    float totalPayment;
+}
