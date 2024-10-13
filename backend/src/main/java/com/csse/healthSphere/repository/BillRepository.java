1aab227154cffa645714af5e1b04c3c0928d4341
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:14:47 +0530

    Bill Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/repository/BillRepository.java
@@ -0,0 +1,10 @@
+package com.csse.healthSphere.repository;
+
+import com.csse.healthSphere.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill, Long> {
+
+}
