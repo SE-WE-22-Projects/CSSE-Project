f00ab23c61960c0fe6f1435c897924868c65f2e6
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:17:08 +0530

    Ward Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/model/Ward.java
@@ -0,0 +1,17 @@
+package com.csse.healthSphere.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Ward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int wardId;
+
+    @OneToMany(mappedBy = "departmentId")
+    private List<MedicalStaff> medicalStaff;
+}
