2d549cf327466d4371b44190511d8e47950332de
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 15:15:54 +0530

    MedicalStaff Model and repository

---
+++ backend/src/main/java/com/csse/healthSphere/repository/MedicalStaffRepository.java
@@ -0,0 +1,8 @@
+package com.csse.healthSphere.repository;
+
+import com.csse.healthSphere.model.MedicalStaff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicalStaffRepository extends JpaRepository<MedicalStaff, Long> {
+
+}
