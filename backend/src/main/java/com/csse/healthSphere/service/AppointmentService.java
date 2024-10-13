469b363a9557d45c5bb119b6a040673360ea4fe9
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-13 20:36:31 +0530

    Appointment service class

--- backend/src/main/java/com/csse/healthSphere/service/AppointmentService.java
+++ backend/src/main/java/com/csse/healthSphere/service/AppointmentService.java
@@ -1,21 +1,35 @@
 package com.csse.healthSphere.service;
 
 import com.csse.healthSphere.dto.AppointmentRequest;
+import com.csse.healthSphere.exception.ResourceNotFoundException;
 import com.csse.healthSphere.model.Appointment;
+import com.csse.healthSphere.model.Patient;
 import com.csse.healthSphere.repository.AppointmentRepository;
+import com.csse.healthSphere.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
+    private final ModelMapper modelMapper;
+    private final PatientRepository patientRepository;
 
     public Appointment createAppointment(AppointmentRequest appointmentRequest){
+        Appointment appointment =modelMapper.map(appointmentRequest, Appointment.class);
+        // fetch patient & check existence
+        Patient patient = patientRepository.findById(appointmentRequest.getPatientId())
+                .orElseThrow(() -> new ResourceNotFoundException("Patient not found"));;
 
-        return null;
+        appointment.setPatient(patient);
+        appointment.setStatus("pending");
+
+        return appointmentRepository.save(appointment);
     }
 
     public List<Appointment> getAllAppointments(){
