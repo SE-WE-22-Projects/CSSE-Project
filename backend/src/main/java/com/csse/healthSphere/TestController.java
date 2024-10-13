9404a28f2dfb76c74d1a3e7a6534dc02c41df4d8
yehan2002 <yehanjaya2002@gmail.com>
2024-10-13 18:48:25 +0530

    Add WebConfig.

--- backend/src/main/java/com/csse/healthSphere/TestController.java
+++ backend/src/main/java/com/csse/healthSphere/TestController.java
@@ -1,14 +1,15 @@
 package com.csse.healthSphere;
 
 import com.csse.healthSphere.repository.MedicalStaffRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class TestController {
-    @Autowired
-    private  MedicalStaffRepository repository;
+    private final MedicalStaffRepository repository;
 
     @GetMapping("/")
     public String index() {
