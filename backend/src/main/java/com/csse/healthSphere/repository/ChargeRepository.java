87dd9f5c06555ec71d3a629c7eeda760cb4a59b3
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-15 23:14:05 +0530

    added required methods to repos

--- backend/src/main/java/com/csse/healthSphere/repository/ChargeRepository.java
+++ backend/src/main/java/com/csse/healthSphere/repository/ChargeRepository.java
@@ -1,10 +1,26 @@
 package com.csse.healthSphere.repository;
 
+import com.csse.healthSphere.model.Appointment;
 import com.csse.healthSphere.model.Charge;
+import com.csse.healthSphere.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChargeRepository extends JpaRepository<Charge, Long> {
+    /**
+     * get list of charges according to appointment
+     * @param appointment
+     * @return
+     */
+    List<Charge> findByAppointment(Appointment appointment);
 
+    /**
+     * get list of charges according to patient
+     * @param patient
+     * @return
+     */
+    List<Charge> findByAppointmentPatient(Patient patient);
 }
--- backend/src/main/java/com/csse/healthSphere/repository/ReportRepository.java
+++ backend/src/main/java/com/csse/healthSphere/repository/ReportRepository.java
@@ -1,10 +1,43 @@
 package com.csse.healthSphere.repository;
 
+import com.csse.healthSphere.model.Admission;
+import com.csse.healthSphere.model.MedicalService;
+import com.csse.healthSphere.model.Patient;
 import com.csse.healthSphere.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    /**
+     * get list of reports according to admission and service
+     * @param admission
+     * @param medicalService
+     * @return
+     */
+    List<Report> findByAdmissionAndMedicalService(Admission admission, MedicalService medicalService);
 
+    /**
+     * get list of reports according to patient and service
+     * @param patient
+     * @param medicalService
+     * @return
+     */
+    List<Report> findByAdmissionAppointmentPatientAndMedicalService(Patient patient, MedicalService medicalService);
+
+    /**
+     * get list of reports according to admission
+     * @param admission
+     * @return
+     */
+    List<Report> findByAdmission(Admission admission);
+
+    /**
+     * get list of reports according to patient
+     * @param patient
+     * @return
+     */
+    List<Report> findByAdmissionAppointmentPatient(Patient patient);
 }
