e222056a6d08964599cb604d3feff18ce318cd1b
yehan2002 <yehanjaya2002@gmail.com>
2024-10-17 19:22:26 +0530

    Finish ward crud.

--- frontend/src/admin/systems/ward/Ward.tsx
+++ frontend/src/admin/systems/ward/Ward.tsx
@@ -1,12 +1,13 @@
 import { GridColDef } from '@mui/x-data-grid'
 import { useEffect, useState } from 'react'
-import { API } from '../../App';
-import { Ward, WardRequest } from '../../api';
-import DataForm, { Field } from '../../shared/components/DataForm';
-import { PageTitle, PageSubtitle } from '../../shared/components/Logo';
 import { useConfirm } from 'material-ui-confirm';
 import { enqueueSnackbar } from 'notistack';
-import StyledDataGrid from '../../shared/components/StyledDataGrid';
+import { Ward, WardRequest } from '../../../api';
+import DataForm, { Field } from '../../../shared/components/DataForm';
+import { API } from '../../../App';
+import { PageSubtitle, PageTitle } from '../../../shared/components/Logo';
+import StyledDataGrid from '../../../shared/components/StyledDataGrid';
+import { Box, Divider, Paper } from '@mui/material';
 
 function sleep(duration: number): Promise<void> {
     return new Promise<void>((resolve) => {
@@ -18,8 +19,8 @@ function sleep(duration: number): Promise<void> {
 
 const GridFields: GridColDef<Ward>[] = [
     { field: 'wardId', headerName: 'ID', width: 90 },
-    { field: 'name', headerName: "Ward Name", width: 200 },
-    { field: 'location', headerName: "Location", width: 200 },
+    { field: 'name', headerName: "Ward Name", width: 200, flex: 1 },
+    { field: 'location', headerName: "Location", width: 200, flex: 1 },
     { field: 'capacity', headerName: "Capacity", width: 90 },
 ]
 
@@ -35,6 +36,7 @@ const WardPage = () => {
     const [wards, setWards] = useState<Ward[]>([]);
     const [editorShown, setEditorShown] = useState(false);
     const [selectedWard, setSelectedWard] = useState<Ward | null>(null);
+    const [loading, setLoading] = useState(true);
 
     const confirm = useConfirm();
 
@@ -42,7 +44,8 @@ const WardPage = () => {
     useEffect(() => {
         (async () => {
             let response = await API.getAllWards()
-            setWards(response.data);
+            setWards([...response.data]);
+            setLoading(false);
         })()
     }, []);
 
@@ -63,8 +66,18 @@ const WardPage = () => {
         await sleep(1000);
         try {
             let response = await API.updateWard(selectedWard?.wardId!, data);
-            setWards([...wards, response.data]);
+
+            // update the state
+            let idx = wards.indexOf(selectedWard!);
+            let newWards = [...wards]
+            newWards[idx] = response.data;
+            setWards(newWards);
+
             enqueueSnackbar("Ward updated successfully", { variant: "success" });
+
+            // close editor
+            setSelectedWard(null);
+            setEditorShown(false);
         } catch (e) {
             enqueueSnackbar("Failed to update ward", { variant: "error" });
             console.error(e);
@@ -72,8 +85,10 @@ const WardPage = () => {
     }
 
     const editWard = async (id: number) => {
+        // find the ward for the given id.
         const ward = wards.find((w) => w.wardId === id)!;
 
+        // display the editor
         setEditorShown(true);
         setSelectedWard(ward);
     }
@@ -85,7 +100,9 @@ const WardPage = () => {
         await confirm({ title: `Permanently delete ward ${ward.name}?` });
         try {
             await API.deleteWard(ward.wardId!);
-            setWards(wards.filter(i => i.wardId != ward.wardId))
+
+            setWards(wards.filter(i => i.wardId != ward.wardId));
+
             enqueueSnackbar("Ward deleted successfully", { variant: "success" });
         } catch (e) {
             enqueueSnackbar("Failed to delete ward", { variant: "error" });
@@ -93,18 +110,42 @@ const WardPage = () => {
         }
     }
 
+    const closeEditor = () => {
+        setSelectedWard(null);
+        setEditorShown(false);
+    }
+
 
 
     return (
         <>
-            <PageTitle >Ward Management</PageTitle>
-            <PageSubtitle>Manage all wards in the hospital</PageSubtitle>
+            <Box sx={{ pb: 2 }}>
+                <PageTitle >Ward Management</PageTitle>
+                <PageSubtitle>Manage all wards in the hospital</PageSubtitle>
+                <Divider />
+            </Box>
+
             {editorShown ?
-                <DataForm fields={FormFields}
-                    onSubmit={selectedWard == null ? createWard : updateWard}
-                    initial={selectedWard ?? undefined}
-                    submitText={selectedWard == null ? "Create" : "Update"} /> :
-                <StyledDataGrid columns={GridFields} rows={wards} getRowId={(v) => v.wardId!} onDelete={deleteWard} onEdit={editWard} />
+                <Paper elevation={2} sx={{ p: "12px" }}>
+                    <PageTitle>{selectedWard == null ? `Create Ward` : `Edit Ward ${selectedWard.name}`}</PageTitle>
+                    <DataForm
+                        sx={{ my: 4 }}
+                        fields={FormFields}
+                        canCancel
+                        onCancel={closeEditor}
+                        onSubmit={selectedWard == null ? createWard : updateWard}
+                        initial={selectedWard ?? undefined}
+                        submitText={selectedWard == null ? "Create" : "Update"} />
+                </Paper>
+                :
+
+                <StyledDataGrid
+                    columns={GridFields}
+                    rows={wards}
+                    getRowId={(v) => v.wardId!}
+                    onDelete={deleteWard}
+                    onEdit={editWard}
+                    loading={loading} />
             }
         </>
     )
