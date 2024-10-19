d8385acb7eb16e08d797395bc8f960e27ce8f1c9
Navindu Nimsara <navidunimsara@gmail.com>
2024-10-19 18:00:42 +0530

    appointment creation

--- frontend/src/user/routes.tsx
+++ frontend/src/user/routes.tsx
@@ -1,9 +1,8 @@
 import { RouteObject } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import Appointment from "./appointment/Appointment";
-import AppointmentList from "./appointment/AppointmentList";
 
 export const Routes: RouteObject[] = [
     { index: true, element: <LandingPage /> },
-    {path: "/appointment", element: <AppointmentList/>}
+    {path: "/appointment", element: <Appointment/>}
 ];
